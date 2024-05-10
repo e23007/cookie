2f421964628bc849dd71996a186a60c77e15633c
e23007 <e23007@std.it-college.ac.jp>
2024-05-10 14:39:50 +0900

    エンドレス

--- cookie.py
+++ cookie.py
@@ -1,4 +1,6 @@
-import pyautogui,sys,keyboard
+import pyautogui
+import sys
+import keyboard
 
 
 # while True:
@@ -7,7 +9,18 @@ import pyautogui,sys,keyboard
 #     else:
 #         pyautogui.click(291,386)
 
-for i in range(50):
-    if keyboard.read_key('q'):
-        sys.exit()
-    pyautogui.click(291,386)
+# for i in range(50):
+#     if keyboard.read_key('q'):
+#         sys.exit()
+#     pyautogui.click(291,386)
+
+
+# for i in range(1000):
+#     pyautogui.click(291,386)
+
+
+try:
+    while True:
+        pyautogui.click(291,386)
+except keyboard.read_key('q'):
+    sys.exit()
